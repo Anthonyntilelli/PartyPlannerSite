3d8090eeca0970b42191642106971ea73beeb8aa
Anthony Tilelli <Anthony@tilelli.me>
2020-02-25 23:39:02 -0500

    updated models

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,8 @@ User.destroy_all
 Venue.destroy_all
 Theme.destroy_all
 Party.destroy_all
+Invite.destroy_all
+Gift.destroy_all
 tom_user = User.create(first_name: "Tom", last_name:"Smith", birthday: "05-01-2001", email: "tom@example.com", password: "pw12345", locked: false, admin: true)
 amy_user = User.create(first_name: "Amy", last_name:"Smith", birthday: "05-01-2002", email: "amy@example.com", password: "pw12345", locked: false, admin: false)
 fl_venue = Venue.create(name: 'Fun Land', location: "1234 East wood", active: true)
