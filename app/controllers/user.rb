32c4e8d2e5576ff70cbaa71b8c3df25da6c98701
Anthony Tilelli <Anthony@tilelli.me>
2020-03-12 23:24:52 -0400

    add user signup page

--- app/controllers/user.rb
+++ app/controllers/user.rb
@@ -2,12 +2,38 @@
 
 # User controller with Sinatra
 class UserController < Sinatra::Base
-  # User can view Profile
-  # User can update Profile
-  # User can signup
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+    use Rack::MethodOverride # Method overwrite
+    enable :sessions
+    set :session_secret, "secret"
+  end
+
   # User can log-in (password less log in also??)
   # User can verify email owernship
   # User can update recover their account, if locked or forgotten password
   # User can logout
-  # User can destroy their account
+  # User can delete their account
+
+  # User can signup
+  get '/user/signup' do
+    erb :"user/signup"
+  end
+
+  post '/user/signup' do
+    # CGI::escapeHTML(params["email"])
+    # Rack::Utils.escape_html("quote ' double quotes \"")
+    # params['name']
+    # binding.pry
+    erb :welcome
+  end
+
+  # User can view Profile
+  get '/user/profile/:id' do
+  end
+
+  # User can update Profile
+  patch '/user/profile/:id' do
+  end
 end
