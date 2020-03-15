401821d1e97e598b59cd2930f72aab6b12c86e29
Anthony Tilelli <Anthony@tilelli.me>
2020-03-14 20:42:42 -0400

    add signup, log in and log out

--- app/controllers/user.rb
+++ app/controllers/user.rb
@@ -11,13 +11,14 @@ class UserController < Sinatra::Base
     register Sinatra::Flash
   end
 
-  # User can log-in (password less log in also??)
+  # TODO: Sanitize Input
+  # passwordless log in also??
+  # User can reset their account
   # User can verify email owernship
   # User can update recover their account, if locked or forgotten password
-  # User can logout
   # User can delete their account
 
-  # User can signup
+  # User signup Page
   get '/user/signup' do
     @title = 'Welcome to the party'
     @header_text = 'Party Planning, inc'
@@ -25,9 +26,9 @@ class UserController < Sinatra::Base
     erb :"user/signup"
   end
 
+  # Create User account
   post '/user/signup' do
     begin
-      # TODO: Sanitize Input
       User.create!(
         name: params['name'],
         email: params['email'],
@@ -39,19 +40,60 @@ class UserController < Sinatra::Base
         admin: false
       )
     rescue ActiveRecord::RecordInvalid => e
-      flash[:SUCCESS] = e.message
-      redirect "/user/signup"
+      flash[:ERROR] = e.message
+      redirect '/user/signup'
     end
     # TODO: Send email validation for user accound
-    flash[:SUCCESS] = "Signup Successfull: Welcome #{params["name"]}, please view your email for confirm link."
+    flash[:SUCCESS] = "Signup Successfull: Welcome #{params['name']}, please view your email for confirm link."
     redirect '/'
   end
 
-  # User can view Profile
-  get '/user/profile/:id' do
+  # User Log-in
+  get '/user/login' do
+    redirect to '/' if session['user_id']
+    @title = 'Welcome to the party'
+    @header_text = 'Party Planning, inc'
+    @follow_up_paragraph = 'Best Place to Schedule your Party!'
+    erb :"user/login"
   end
 
-  # User can update Profile
-  patch '/user/profile/:id' do
+  # Authenticate User
+  post '/user/login' do
+    user = User.find_by(email: params['email'])
+    if user&.authenticate(params['password'])
+      if !user.locked && user.valid_email
+        session['user_id'] = user.id
+        session['login_time'] = Time.now.utc.to_s
+        redirect to '/', 200
+      else
+        session['user_id'] = nil
+        session['login_time'] = nil
+        flash[:ERROR] = 'Email is locked or email is not verified, please click \'Forgot my password\' to unlock'
+        redirect to '/user/login', 401
+      end
+    else
+      flash[:ERROR] = 'Incorrect User or Password'
+      redirect to '/user/login', 401
+    end
+    halt 404
   end
+
+  # Logout and clears session
+  get '/user/logout' do
+    session.clear
+    flash[:SUCCESS] = 'You are Logged out'
+    redirect to '/', 200
+  end
+
+  # # User can view Profile
+  # get '/user/profile/:id' do
+  # end
+
+  # # User can update Profile
+  # patch '/user/profile/:id' do
+  # end
+
+  # Reset Password
+  # get '/user/forgot_password' do
+  # end
 end
