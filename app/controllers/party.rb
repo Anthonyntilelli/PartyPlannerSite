574d321f6a7e5bc62a600b79368f0925f520a792
Anthony Tilelli <Anthony@tilelli.me>
2020-04-09 20:50:56 -0400

    continue work on party controller and related.

--- app/controllers/party.rb
+++ app/controllers/party.rb
@@ -6,6 +6,32 @@ class PartyController < ApplicationController
   get '/post_auth/party/new' do
     erb :'party/new'
   end
+
+  post '/post_auth/party/new' do
+    user = load_user_from_session
+    begin
+      party = Party.create!(
+        name: params['name'],
+        user: user,
+        venue: Venue.find_by_id!(params['venue']),
+        theme: Theme.find_by_id!(params['theme']),
+        event_date: params['partydate'], # yyyy-mm-dd
+        time_slot: params['time_slot']
+      )
+    rescue ActiveRecord::RecordInvalid => e
+      flash[:ERROR] = e.message
+      redirect to '/post_auth/party/new', 400
+    end
+    flash[:SUCCESS] = 'Party Successfully Created: We will see you there!'
+    redirect to "/post_auth/party/#{party.id}"
+  end
+
+  # View Party
+  get '/post_auth/party/:party_id' do end
+
+  # Edit party (E.g. Change Venue,date, time)
+  get '/post_auth/party/:party_id/edit' do end
+
   # Delete Party
-  # Edit Party (E.g. Change Venue,date, time)
+  delete '/post_auth/party/:party_id' do end
 end
