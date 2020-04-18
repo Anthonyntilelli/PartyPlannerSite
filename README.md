76cbfabea45d35f05c614e44e86b3fddab80371d
Anthony Tilelli <Anthony@tilelli.me>
2020-04-17 20:23:05 -0400

    add install instructions

--- README.md
+++ README.md
@@ -3,6 +3,20 @@
 Sinatra powered site for planning events and inviting other users.
 This is the second Learn.co project for flatiron bootcamp.
 
+## Install from Source
+
+Project is installed via [Bundler](https://bundler.io/)
+Project is coded for development mode
+
+1. clone project
+2. run `bundle install`
+3. create `party.env` from `party.env.example`
+    - see `ENV config` section for more info
+4. source `party.env`
+5. Create database with `rake db:create:all`
+6. Set up with `rake db:migrate` and `rake db:seed`
+7. Run `shotgun` to start webserver.
+
 ## ENV configs
 
 - Required:
@@ -10,9 +24,14 @@ This is the second Learn.co project for flatiron bootcamp.
   - `PARTY_SENDGRID_EMAIL` - Send grid TO email address
   - `PARTY_HMAC_URl_KEY` - used for hmac url key, generate via securerandom, must be 64 bits
   - `PARTY_SESSION_KEY`  - used for session secret, generate via securerandom, must be 64 bits
-- Debug:
+- Debug (OPTIONAL):
   - `PARTY_DISABLE_DNS` - set to  `'YES'` to disable DNS checks for user email.
   - `PARTY_DISABLE_EMAIL` - set to `'YES'` for email request to silently be ignored.
+
+## SENDGRID
+
+Project uses Send Grid to manage emails for user account creation and passwordless logins.
+The api only need enough access to send email. Full access is not needed or recommended.
 
 ## Routes
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,31 @@ Theme.destroy_all
 Party.destroy_all
 Invite.destroy_all
 
-puts 'Active Venue'
+puts 'Active Venue(1)'
 fl_venue = Venue.create!(
   name: 'Fun Land',
   zipcode: 12_345,
   state: 'NY',
   city: 'New York',
   street_addr: '1234 My Street',
+  active: true
+)
+puts 'Active Venue(2)'
+Venue.create!(
+  name: 'Central Dance Ball',
+  zipcode: 12_348,
+  state: 'NY',
+  city: 'New York',
+  street_addr: '1346 Great Street',
+  active: true
+)
+puts 'Active Venue(3)'
+Venue.create!(
+  name: 'Rocken Rave Central',
+  zipcode: 12_850,
+  state: 'NY',
+  city: 'New York',
+  street_addr: '1092 Super Ave',
   active: true
 )
 puts 'Inactive Venue'
@@ -24,49 +42,56 @@ Venue.create!(
   street_addr: '1234 My Other Street',
   active: false
 )
-puts 'Active Theme'
+puts 'Active Theme(1)'
 sp_theme = Theme.create!(name: 'Space', active: true)
+puts 'Active Theme(2)'
+Theme.create!(name: 'Princess', active: true)
+puts 'Active Theme(3)'
+Theme.create!(name: 'Wild West', active: true)
 puts 'Inactive Theme'
 Theme.create!(name: 'Nature', active: false)
-puts 'Tom User'
-tom_user = User.create!(
-  name: 'Tom Smith',
-  email: 'tom@example.com',
-  email_confirmation: 'tom@example.com',
-  password: 'pw123456',
-  password_confirmation: 'pw123456',
-  allow_passwordless: false,
-  locked: false,
-  admin: true
-)
-puts 'Amy User'
-amy_user = User.create!(
-  name: 'Amy Smith',
-  email: 'amy@example.com',
-  email_confirmation: 'amy@example.com',
-  password: 'pw123456',
-  password_confirmation: 'pw123456',
-  locked: false,
-  allow_passwordless: true,
-  admin: false
-)
-puts 'Tom Party'
-tm_party = Party.create!(
-  name: 'Super Party',
-  user: tom_user,
-  venue: fl_venue,
-  theme: sp_theme,
-  event_date: '2020-05-02', # yyyy-mm-dd
-  time_slot: 4
-)
-puts 'Amy Party (same name)'
-Party.create!(
-  name: 'Super party',
-  user: amy_user,
-  venue: fl_venue,
-  theme: sp_theme,
-  event_date: '2020-05-02', # yyyy-mm-dd
-  time_slot: 3
-)
-puts 'Tom invites Amy'
-Invite.create!(user: amy_user, party: tm_party, status: 'pending')
+
+if ENV['SINATRA_ENV'] == 'development'
+  puts 'Tom User'
+  tom_user = User.create!(
+    name: 'Tom Smith',
+    email: 'tom@example.com',
+    email_confirmation: 'tom@example.com',
+    password: 'pw123456',
+    password_confirmation: 'pw123456',
+    allow_passwordless: false,
+    locked: false,
+    admin: true
+  )
+  puts 'Amy User'
+  amy_user = User.create!(
+    name: 'Amy Smith',
+    email: 'amy@example.com',
+    email_confirmation: 'amy@example.com',
+    password: 'pw123456',
+    password_confirmation: 'pw123456',
+    locked: false,
+    allow_passwordless: true,
+    admin: false
+  )
+  puts 'Tom Party'
+  tm_party = Party.create!(
+    name: 'Super Party',
+    user: tom_user,
+    venue: fl_venue,
+    theme: sp_theme,
+    event_date: '2020-05-02', # yyyy-mm-dd
+    time_slot: 4
+  )
+  puts 'Amy Party (same name)'
+  Party.create!(
+    name: 'Super party',
+    user: amy_user,
+    venue: fl_venue,
+    theme: sp_theme,
+    event_date: '2020-05-02', # yyyy-mm-dd
+    time_slot: 3
+  )
+  puts 'Tom invites Amy'
+  Invite.create!(user: amy_user, party: tm_party, status: 'pending')
+end
